85b2128f7118be0e67c250b92303fda27b545489
Eli Chen <ec063q@intl.att.com>
2017-11-14 17:53:11 +0200

    n1

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,3 +1,22 @@
-export default {
-  
+import {NgModule} from "@angular/core";
+import {FormElementsModule} from "./form-elements/form-elements.module";
+import {ButtonsModule} from "./buttons/buttons.module";
+import {ModalModule} from "./modals/modal.module";
+import {PopupMenuModule} from "./popup-menu/popup-menu.module";
+
+@NgModule({
+    imports: [
+        ModalModule,
+        FormElementsModule,
+        ButtonsModule,
+        PopupMenuModule
+    ],
+    exports: [
+        ModalModule,
+        FormElementsModule,
+        ButtonsModule,
+        PopupMenuModule
+    ]
+})
+export default class ComponentsModule {
 }
--- stories/ng2-component-lab/components.module.ts
+++ stories/ng2-component-lab/components.module.ts
@@ -1,13 +1,10 @@
 import {NgModule, Injector} from "@angular/core";
+import UiAngularModule from "../../src/angular/index";
 import {KeysPipe} from "./utils/pipes/keys.pipe";
 import {CommonModule} from "@angular/common";
 import {ColorsTable} from "./components/colors-table.component";
-import {FormElementsModule} from "../../src/angular/form-elements/form-elements.module";
-import {ButtonsModule} from "../../src/angular/buttons/buttons.module";
 import {ButtonModalExample} from "./components/button-modal-example.component";
-import {ModalModule} from "../../src/angular/modals/modal.module";
 import {InnerContent} from "./components/inner-content-example.component";
-import {PopupMenuModule} from "../../src/angular/popup-menu/popup-menu.module";
 
 @NgModule({
     declarations: [
@@ -17,21 +14,14 @@ import {PopupMenuModule} from "../../src/angular/popup-menu/popup-menu.module";
         KeysPipe
     ],
     imports: [
-        ModalModule,
         CommonModule,
-        FormElementsModule,
-        ButtonsModule,
-        PopupMenuModule
+        UiAngularModule
     ],
     entryComponents: [InnerContent],
     exports: [
         InnerContent,
-        ModalModule,
         ColorsTable,
-        ButtonModalExample,
-        FormElementsModule,
-        ButtonsModule,
-        PopupMenuModule
+        ButtonModalExample
     ],
     providers: [KeysPipe]
 })
